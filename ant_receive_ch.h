195511412750d1d32e94e2cc92827144f3b244a8
vincent golle <v.golle@isispace.nl>
2022-08-17 17:36:24 +0200

    Working program

---
+++ ant_receive_ch.h
@@ -0,0 +1,70 @@
+//
+// Created by vgol on 17/08/2022.
+//
+
+#ifndef ANT_TEST_ANT_RECEIVE_CH_H
+#define ANT_TEST_ANT_RECEIVE_CH_H
+
+#include "types.h"
+#include "ant_receive_defines.h"
+#include "dsi_framer_ant.hpp"
+//#include "dsi_thread.h"
+#include "dsi_serial_generic.hpp"
+//#include "dsi_debug.hpp"
+#include <stdio.h>
+//#include <stdlib.h>
+#include <assert.h>
+//#include <string.h>
+
+class ANTrxService {
+public:
+	ANTrxService(DSISerialGeneric* serialObject, std::function<void(UCHAR *p_aucData)> callback);
+	virtual ~ANTrxService();
+	BOOL Init(sANTrxServiceInit sInit);
+	void Start();
+	void Close();
+
+private:
+	BOOL InitANT();
+
+	// Starts the Message thread.
+	static DSI_THREAD_RETURN RunMessageThread(void *pvParameter_);
+
+	// Listens for a response from the module
+	void MessageThread();
+
+	// Decodes the received message
+	void ProcessMessage(ANT_MESSAGE stMessage, USHORT usSize_);
+
+	// Print user menu
+	void PrintMenu();
+
+	// Detect transmitter device type: current or legacy
+	void DetectDevice(UCHAR &ucDeviceType_, BOOL &bOldToggleBit_, UCHAR &ucToggleAttempts_, BOOL bToggleBit);
+
+	// Network variables
+	UCHAR ucTransType;
+	UCHAR ucAntChannel;
+	UCHAR ucDeviceType;
+	UCHAR ucNetworkNum;
+	USHORT usDeviceNum;
+	USHORT usMessagePeriod;
+
+	BOOL bBursting; // Holds whether the bursting phase of the test has started
+	BOOL bBroadcasting;
+	BOOL bMyDone;
+	BOOL bDone;
+	UCHAR ucChannelType;
+	DSISerialGeneric* pclSerialObject;
+	DSIFramerANT* pclMessageObject;
+	DSI_THREAD_ID uiDSIThread;
+	DSI_CONDITION_VAR condTestDone;
+	DSI_MUTEX mutexTestDone;
+
+	std::function<void(UCHAR *p_aucData)> msg_callback;
+
+	BOOL bDisplay;
+	BOOL bProcessedData;
+};
+
+#endif //ANT_TEST_ANT_RECEIVE_CH_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,92 @@
-#include <iostream>
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+#include <cassert>
+#include "dsi_serial_generic.hpp"
+#include "ant_receive_ch.h"
+
+static void _pw1_callback(UCHAR *p_aucData) {
+
+	UCHAR ucPageNum = p_aucData[0];
+
+	printf("Page: %d received\n", ucPageNum);
+
+	// Page specific data
+	switch(ucPageNum) // Removing the toggle bit
+	{
+		case 0:
+
+			break;
+
+		default:
+
+			break;
+
+	}
+}
+
+static void _pw2_callback(UCHAR *p_aucData) {
+
+	UCHAR ucPageNum = p_aucData[0];
+
+	printf("Page: %d received\n", ucPageNum);
+
+	// Page specific data
+	switch(ucPageNum) // Removing the toggle bit
+	{
+		case 0:
+
+			break;
+
+		default:
+
+			break;
+
+	}
+}
+
+int main(int argc, char *argv[]) {
+
+	// Create Serial object.
+	DSISerialGeneric* pclSerialObject = new DSISerialGeneric();
+	assert(pclSerialObject);
+
+	ANTrxService *pPWR1srv = new ANTrxService(pclSerialObject, _pw1_callback);
+	assert(pPWR1srv);
+
+	sANTrxServiceInit sInit1 = {
+			.ucAntChannel = 0,
+			.ucTransType = 5,
+			.ucDeviceType = 120, // HRM
+			.usDeviceNum = 1234, // TODO
+			.usMessagePeriod = 8192, // TODO
+	};
+
+	ANTrxService *pPWR2srv = new ANTrxService(pclSerialObject, _pw2_callback);
+	assert(pPWR2srv);
+
+	sANTrxServiceInit sInit2 = {
+			.ucAntChannel = 0,
+			.ucTransType = 5,
+			.ucDeviceType = 120, // HRM
+			.usDeviceNum = 1234, // TODO
+			.usMessagePeriod = 8192, // TODO
+	};
+
+	int ret = 0;
+
+	if(ret == 0 && pPWR1srv->Init(sInit1))
+		pPWR1srv->Start();
+	else {
+		delete pPWR1srv;
+		delete pPWR2srv;
+		delete pclSerialObject;
+		ret--;
+	}
+
+	if(ret == 0 && pPWR2srv->Init(sInit2))
+		pPWR2srv->Start();
+	else {
+		ret--;
+	}
+
+	return ret;
 }
